68854dc08591a1555e1b1f444ce6a66c69831b4c
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2020-09-11 16:07:38 +0300

    commit changes remoed comments for hosting

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-// install service worker
-
-const staticCacheName = 'site-static-v2';
-const dynamicCache = 'site-dynamic-v1';
+const staticCacheName = 'site-static-v1';
 const assets = [
   '/',
   'index.html',
   'app.js',
   'styles.css',
-  'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=341f3a5ad73fcf20c2dee19a9f0b6b90&units${x},${y}&units=metric'
+  'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=341f3a5ad73fcf20c2dee19a9f0b6b90&units${x},${y}&units=metric',
+  'http://openweathermap.org/img/wn/01d@2x.png'
 ];
 
 self.addEventListener('install', evt => {
@@ -18,8 +16,6 @@ self.addEventListener('install', evt => {
     })
   )
 });
-
-// activate sw
 
 self.addEventListener('activate', evt => {
   evt.waitUntil(
@@ -33,18 +29,11 @@ self.addEventListener('activate', evt => {
   )
 });
 
-// fetch event
-
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request)
       .then(cacheRes => {
-        return cacheRes || fetch(evt.request).then(fetchRes => {
-          return caches.open(dynamicCache).then(cache => {
-            cache.put(evt.request.url, fetchRes.clone());
-            return fetchRes;
-          })
-        })
+        return cacheRes || fetch(evt.request);
       })
   )
 });
