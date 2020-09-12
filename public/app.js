e9db66a1035ccded3fbf1133bc5bd785ac6e6b7b
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2020-09-12 17:35:01 +0300

    replaced public folder with another one containing  modified files

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
-/ get input element in the header by class name
+// get input element in the header by class name
 const input = document.querySelector(".cities");
+const msg = document.querySelector(".erro-msg");
+var inputVal;
 
 // initiate a window event trigerd after the browser loads
 window.addEventListener("load", () => {
@@ -12,7 +14,7 @@ window.addEventListener("load", () => {
       x = position.coords.longitude;
       y = position.coords.latitude;
 
-      const api = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=341f3a5ad73fcf20c2dee19a9f0b6b90&units${x},${y}&units=metric`;
+      let api = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=341f3a5ad73fcf20c2dee19a9f0b6b90&coords=${x},${y}&units=metric`;
 
       fetchData(api);
     })
@@ -22,39 +24,60 @@ window.addEventListener("load", () => {
 // add an event that in order to get users input to display weather data according to their search input
 input.addEventListener('keypress', e => {
   if(e.keyCode == 13) {
-    getInputValue(input.value);
+    inputVal = input.value;
+    getInputValue(inputVal);
   }
 });
  
 function getInputValue(query) {
-  const api = `http://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=341f3a5ad73fcf20c2dee19a9f0b6b90&units=metric`;
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=341f3a5ad73fcf20c2dee19a9f0b6b90&units=metric`;
 
   fetchData(api);
 }
 
 // fetch data function to enter the returned wether data in the DOM
 function fetchData(url) {
-  let time = document.querySelector(".location > p");
-  let date = document.querySelector(".date");
   let todayDateTime = new Date();
-  let city = document.querySelector("h2");
-  let temp = document.querySelector(".temp");
-  let icon = document.querySelector(".temperature > span img");
-  let description =document.querySelector(".temperature-description");
 
   fetch(url) 
     .then(response => {
       return response.json();
     })
     .then(data => {
-      time.textContent = timeManage(todayDateTime);
-      date.textContent = dateManage(todayDateTime);
-      city.textContent = data.name;
-      temp.textContent = Math.floor(data.main.temp);
-      description.textContent = data.weather[0].description;
-      icon.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-      icon
+      const { main, name, weather } = data; //declaring data object
+      const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@4x.png`;
+      const div = document.querySelector(".dynamic-content");
+      const divContent = `<div class="weather-data">
+        <div class="location">
+          <h2>${name}</h2>
+          <p>${timeManage(todayDateTime)}</p>
+        </div>
+        <div class="temperature-data">
+          <p class="date">${dateManage(todayDateTime)}</p>
+          <div class="temperature">
+            <p><span class="temp">${Math.floor(main.temp)}</span>&#8451;</p>
+            <span><img src="${icon}"></span>
+          </div>
+          <p class="temperature-description">${weather[0].description}</p>
+        </div>
+      </div>`;
+
+      div.innerHTML = divContent;
+
     })
+    .catch(() => {
+      msg.textContent = "Please enter a valid city or check whether you are online";
+    });
+
+    let searchHistory = JSON.parse(localStorage.getItem("searchinput")) || [];
+    searchHistory.push(inputVal);
+  
+    
+    localStorage.setItem('searchinput', JSON.stringify(searchHistory));
+    
+    var lastSearch = JSON.parse(localStorage.getItem("searchinput"));
+    console.log(lastSearch);
+    
 }
 
 // add date in the DOM acording to the format month,date,year
@@ -77,7 +100,7 @@ function timeManage(arg) {
 
 // registering service worker
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then((reg) => {return reg})
-    .catch((err) => {return err})
+  navigator.serviceWorker.register('/sw.js')
+    .then((reg) => {console.log('registerd'), reg})
+    .catch((err) => {console.log('not registerd'), err})
 }
--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,55 @@
+'use strict';
+
 const staticCacheName = "site-static-v1";
-const assets = ["/", "index.html", "app.js", "styles.css", "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=341f3a5ad73fcf20c2dee19a9f0b6b90&units${x},${y}&units=metric", "http://openweathermap.org/img/wn/01d@2x.png"];
+const assets = [
+    "/",
+    "index.html",
+    "app.js",
+    "styles.css",
+    "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=341f3a5ad73fcf20c2dee19a9f0b6b90&units${x},${y}&units=metric",
+    "https://openweathermap.org/img/wn/01d@2x.png"
+];
+
 self.addEventListener("install", (evt) => {
     evt.waitUntil(
         caches.open(staticCacheName).then((cache) => {
-            cache.addAll(assets);
+            return cache.addAll(assets);
         })
     );
     self.skipWaiting();
 });
+
 self.addEventListener("activate", (evt) => {
     evt.waitUntil(
         caches.keys().then((keys) => {
-            return Promise.all(keys.filter((key) => key !== staticCacheName).map((key) => caches.delete(key)));
+            return Promise.all(keys.map((key) => {
+                if (key !== staticCacheName) {
+                    return caches.delete(key);
+                }
+            }))
         })
     );
     self.clients.claim();
 });
+
 self.addEventListener("fetch", (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
     evt.respondWith(
-        caches.match(evt.request).then((cacheRes) => {
-            return cacheRes || fetch(evt.request);
-        })
+        caches.match(e.request)
+            .then((response) => {
+                if(response) {
+                    return response;  
+                }
+                
+                return fetch(e.request).then((responce) => {
+                    return caches.open(staticCacheName).then((cache) => {
+                        cache.put(e.request.url, responce.clone());
+                        return responce;
+                    });
+                });
+            })
+            .catch((error)=> {
+                console.error('error installing service worker', error);
+            })
     );
 });
